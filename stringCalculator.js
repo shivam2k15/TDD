bfdadcce911da320bf3ba0e11bdb8622d060a609
shivam2k15 <shivamkumar2k16k@gmail.com>
2025-03-31 23:21:30 +0530

    testcase for string that supports different delimiters passed

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,12 @@
 const stringCalculator = (str) => {
-str=str.replace(/\n/g,",");
+  let delimiters = ",";
+  if (str.length > 2 && str.slice(0, 2) == "//") {
+    delimiters = str[2];
+    str = str.slice(3);
+  }
+  str = str.replace(/\n/g, delimiters);
   return str
-    .split(",")
+    .split(delimiters)
     .map(Number)
     .reduce((init, item) => init + item, 0);
 };

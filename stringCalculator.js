6a6aa25ce5576f44ebbcc1669b2c49075154b0f2
shivam2k15 <shivamkumar2k16k@gmail.com>
2025-04-01 01:42:11 +0530

    testcase passed with input string if delimiters can be of any length

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,8 +1,26 @@
+const getDelimiter = (str) => {
+  let i = 3,
+    delimiters = "";
+  if (str[i - 1] == "[") {
+    while (i < str.length) {
+      if (str[i] != str[3]) {
+        break;
+      }
+      delimiters += str[i];
+      i += 1;
+    }
+  } else {
+    delimiters = str[i - 1];
+  }
+  return { delLength: i, delimiters };
+};
+
 const stringCalculator = (str) => {
   let delimiters = ",";
   if (str.length > 2 && str.slice(0, 2) == "//") {
-    delimiters = str[2];
-    str = str.slice(3);
+    const { delLength, delimiters: newDelimeters } = getDelimiter(str);
+    delimiters = newDelimeters;
+    str = str.slice(delLength);
   }
   str = str.replace(/\n/g, delimiters);
   let integers = str.split(delimiters);
